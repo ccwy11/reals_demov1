fdc7b0a6fb0382ec47d68c092d1300350ac8fdf8
wychen <ccwy1120@hotmail.com>
2025-10-21 10:20:55 +0800

    auth page fix

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,5 +1,10 @@
 import { createAuthClient } from "better-auth/react"
+import { config } from 'dotenv';
+
+// config({ path: '.env' });
+
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "prcoess.env.NEXT_PUBLIC_BASE_URL"
+    baseURL:   "http://localhost:3000",
+     //   process.env.DATABASE_URL,
 })
