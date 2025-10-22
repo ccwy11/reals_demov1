4230fef25d1a90812bfa5ffb0caf7f0b3376c6f2
wychen <ccwy1120@hotmail.com>
2025-10-22 13:05:55 +0800

    auth reconnect

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,8 @@
-// import { Logout } from "@/components/logout"
+import { Logout } from "@/components/logout"
 export default function Dashboard() {
     return (
         <div className="flex flex-col items-center justify-center gap-6">
-            {/* <Logout /> */}
+            <Logout />
         </div>
 
     )
