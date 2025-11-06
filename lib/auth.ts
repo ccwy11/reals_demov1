145e7a3c2083b7857106a8a976182f5ce43ab120
wychen <ccwy1120@hotmail.com>
2025-11-06 10:52:25 +0800

    auth.ts fix for neon db update

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,26 +1,56 @@
+// import { betterAuth } from "better-auth";
+// import { drizzleAdapter } from "better-auth/adapters/drizzle";
+// import { db } from "../db/drizzle"; // your drizzle instance
+// import { nextCookies } from "better-auth/next-js";
+// import { schema } from "../db/schema";
+
+// export const auth = betterAuth({
+//     emailAndPassword: {
+//         enabled: true,
+//     },
+//     socialProviders: {
+//         google: {
+//             clientId: process.env.GOOGLE_CLIENT_ID as string,
+//             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+//         },
+//     },
+//     database: drizzleAdapter(db, {
+//         provider: "pg",
+//         schema,// or "mysql", "sqlite"
+//     }),
+
+//     plugins: [nextCookies()]
+
+// });
+
+
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { db } from "../db/drizzle"; // your drizzle instance
 import { nextCookies } from "better-auth/next-js";
-import { schema } from "../db/schema";
+import { schema } from "@/db/schema";
 
-export const auth = betterAuth({
-    emailAndPassword: {
-        enabled: true,
-    },
-    socialProviders: {
-        google: {
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+const createAuth = () => { 
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
+    const { db } = require("../db/drizzle"); // your drizzle instance
+
+    return betterAuth({
+        emailAndPassword: {
+            enabled: true,
         },
-    },
-    database: drizzleAdapter(db, {
-        provider: "pg",
-        schema,// or "mysql", "sqlite"
-    }),
+        socialProviders: {
+            google: {
+                clientId: process.env.GOOGLE_CLIENT_ID as string,
+                clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+            },
+        },
+        database: drizzleAdapter(db, {
+            provider: "pg",
+            schema,// or "mysql", "sqlite"
+        }),
 
-    plugins: [nextCookies()]
+        plugins: [nextCookies()]
 
-});
+    });
+}
 
-
+export const auth = createAuth();
