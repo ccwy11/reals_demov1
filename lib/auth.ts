eef2db5c5fef74e24c9b41e446bb37a74a219967
wychen <ccwy1120@hotmail.com>
2025-11-06 10:56:21 +0800

    auth.ts fix

--- lib/auth.ts
+++ lib/auth.ts
@@ -29,28 +29,23 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { nextCookies } from "better-auth/next-js";
 import { schema } from "@/db/schema";
 
-const createAuth = () => { 
+export const getAuthHandler = () => { 
     // eslint-disable-next-line @typescript-eslint/no-require-imports
     const { db } = require("../db/drizzle"); // your drizzle instance
+const authInstance = betterAuth({
+    emailAndPassword: { enabled: true },
+    socialProviders: {
+      google: {
+        clientId: process.env.GOOGLE_CLIENT_ID as string,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      },
+    },
+    database: drizzleAdapter(db, {
+      provider: "pg",
+      schema,
+    }),
+    plugins: [nextCookies()],
+  });
 
-    return betterAuth({
-        emailAndPassword: {
-            enabled: true,
-        },
-        socialProviders: {
-            google: {
-                clientId: process.env.GOOGLE_CLIENT_ID as string,
-                clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-            },
-        },
-        database: drizzleAdapter(db, {
-            provider: "pg",
-            schema,// or "mysql", "sqlite"
-        }),
-
-        plugins: [nextCookies()]
-
-    });
-}
-
-export const auth = createAuth();
+  return authInstance;
+};
