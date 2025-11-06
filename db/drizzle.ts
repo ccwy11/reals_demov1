145ff49de3e21ca1f06e9bedb7b1041ead09147b
wychen <ccwy1120@hotmail.com>
2025-11-06 10:36:02 +0800

    neon fix

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -1,7 +1,17 @@
-import { config } from "dotenv";
-import { drizzle } from 'drizzle-orm/neon-http';
-import { schema } from '../db/schema';
+// import { config } from "dotenv";
+// import { drizzle } from 'drizzle-orm/neon-http';
+// import { schema } from '../db/schema';
 
-config({ path: ".env" }); // or .env.local
+// config({ path: ".env" }); // or .env.local
 
-export const db = drizzle(process.env.DATABASE_URL!, { schema });
+// export const db = drizzle(process.env.DATABASE_URL!, { schema });
+
+
+import { neon, neonConfig } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+import * as schema from "@/db/schema";
+
+neonConfig.fetchConnectionCache = true;
+
+const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle(sql, { schema });
--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,10 +1,8 @@
 import { createAuthClient } from "better-auth/react"
-import { config } from 'dotenv';
 
-// config({ path: '.env' });
 
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL:   "http://localhost:3000",
-     //   process.env.DATABASE_URL,
+    baseURL: process.env.NEXT_PUBLIC_BASE_URL ||  "http://localhost:3000",
+
 })
